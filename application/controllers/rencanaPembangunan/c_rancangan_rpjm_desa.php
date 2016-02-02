f125555c6071b843a2981b5237516c9465dcea33
hajigadung <hajigadung@gmail.com>
2016-02-02 20:05:24 +0700

    menambahkan fungsi import excel

--- application/controllers/rencanaPembangunan/c_rancangan_rpjm_desa.php
+++ application/controllers/rencanaPembangunan/c_rancangan_rpjm_desa.php
@@ -1,69 +1,86 @@
 <?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-include_once APPPATH.'controllers/rencanaPembangunan/c_baseRencanaPembangunan.php';
+include_once APPPATH . 'controllers/rencanaPembangunan/c_baseRencanaPembangunan.php';
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-
-
 class C_rancangan_rpjm_desa extends C_baseRencanaPembangunan {
+
     function __construct() {
         parent::__construct('Rancangan RPJM Desa', 'v_rencanaPembangunan');
-        
+
         $this->load->helper('flexigrid_helper');
     }
-    
-    public function index(){
-        
+
+    public function index() {
+
         $session['hasil'] = $this->session->userdata('logged_in');
         $role = $session['hasil']->role;
-        
-        
-        if($role != 'Perencana Pembangunan'){
+
+
+        if ($role != 'Perencana Pembangunan') {
             redirect('c_login', 'refresh');
         }
-        
+
         $this->lists();
     }
-    
-    public function lists(){
-        $colModel['id_rpjmd'] = array('ID', 30, TRUE, 'center', 0);
-        $colModel['program'] = array('Program Kegiatan', 220, TRUE, 'left', 2);
-        $colModel['kondisi_awal'] = array('Kondisi Awal', 220, TRUE, 'left', 2);
-        $colModel['target'] = array('Target', 220, TRUE, 'left', 2);
-        $colModel['id_tahun_anggaran'] = array('Tahun Anggaran', 120, TRUE, 'center', 2);
-        $colModel['aksi'] = array('AKSI', 120, FALSE, 'center', 0);
 
-        //Populate flexigrid buttons..
-        $buttons[] = array('Select All', 'check', 'btn');
-        $buttons[] = array('separator');
-        $buttons[] = array('DeSelect All', 'uncheck', 'btn');
-        $buttons[] = array('separator');
-        $buttons[] = array('Add', 'add', 'btn');
-        $buttons[] = array('separator');
-        $buttons[] = array('Delete Selected Items', 'delete', 'btn');
-        $buttons[] = array('separator');
+    public function load_data() {
+        $this->load->library('flexigrid');
+        $valid_fields = array('id_rpjmd');
 
-        $gridParams = array(
-            'height' => 300,
-            'rp' => 10,
-            'rpOptions' => '[10,20,30,40]',
-            'pagestat' => 'Displaying: {from} to {to} of {total} items.',
-            'blockOpacity' => 0.5,
-            'title' => '',
-            'showTableToggleBtn' => false
-        );
+        $this->flexigrid->validate_post('id_rancangan_rpjm_desa', 'ASC', $valid_fields);
+    }
 
-        $grid_js = build_grid_js('flex1', site_url('rencanaPembangunan/c_rpjmd/load_data'), $colModel, 'id_rpjmd', 'asc', $gridParams, $buttons);
+    public function lists() {
+
+        $this->config->load('rp_rancangan_rpjm_desa');
+        $r_rpjm_desa_config = $this->config->item('rp_rancangan_rpjm_desa');
+
+        $colModel = $r_rpjm_desa_config['colModel'];
+        $buttons = $r_rpjm_desa_config['buttons'];
+        $gridParams = $r_rpjm_desa_config['gridParams'];
+
+        $grid_js = build_grid_js('flex1', site_url('rencanaPembangunan/c_rancangan_rpjm_desa/load_data'), $colModel, 'id_rancangan_rpjm_desa', 'asc', $gridParams, $buttons);
 
         $this->set('js_grid', $grid_js);
         $this->set('deskripsi_title', 'Rencana Pembangunan Jangka Menengah Daerah');
     }
+
+    public function import_excel() {
+        $config['upload_path'] = "./uploads/temp_upload_excel/rpjm/";
+        $config['allowed_types'] = "xls|xlsx";
+
+        $this->load->library('upload', $config);
+
+//        var_dump($this->upload->validate_upload_path());exit;
+
+
+        if ($this->session->userdata('logged_in')) {
+            $this->load->library('rencanaPembangunan/l_read_rancangan_rpjm_desa');
+
+            $is_upload_ok = $this->l_read_rancangan_rpjm_desa->upload_excel('file_excel');
+            $save_content_status = $this->l_read_rancangan_rpjm_desa->save_content_excel();
+            
+            if($save_content_status){
+                redirect('rencanaPembangunan/c_rancangan_rpjm_desa', 'refresh');
+            }else{
+                
+            }
+        } else {
+            redirect('c_login', 'refresh');
+        }
+    }
+
+    public function add() {
+        
+    }
+
 }
