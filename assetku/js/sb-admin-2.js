74328da310447679d436850cb269233d0dc7477f
hajigadung <hajigadung@gmail.com>
2016-02-16 11:04:52 +0700

    check matis menu, jika ada maka panggil, jika tidak ada maka hiraukan

--- assetku/js/sb-admin-2.js
+++ assetku/js/sb-admin-2.js
@@ -1,14 +1,18 @@
-$(function() {
+$(function () {
 
-    $('#side-menu').metisMenu();
+    try {
+        $('#side-menu').metisMenu();
+    } catch (err) {
+
+    }
 
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
-    $(window).bind("load resize", function() {
+$(function () {
+    $(window).bind("load resize", function () {
         topOffset = 50;
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -20,7 +24,8 @@ $(function() {
 
         height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
         height = height - topOffset;
-        if (height < 1) height = 1;
+        if (height < 1)
+            height = 1;
         if (height > topOffset) {
             $("#page-wrapper").css("min-height", (height) + "px");
         }
