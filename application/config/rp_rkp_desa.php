699fdcd150f438c797f6149cfc0cf43bde10a5ec
hajigadung <hajigadung@gmail.com>
2016-02-16 11:01:01 +0700

    config flexigrid RKP

--- application/config/rp_rkp_desa.php
+++ application/config/rp_rkp_desa.php
@@ -4,38 +4,27 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 
-$config['array_total_bidang'] = array(
-    21 => "total_bidang_1",
-    22 => "total_bidang_2",
-    23 => "total_bidang_3",
-    24 => "total_bidang_4",
-);
 /**
  * 
  * array('title', 'lebar kolom', ?, 'text alignment', ?)
  * array('Tahun Anggaran', 120, TRUE, 'center', 2);
  */
-$config['rp_rancangan_rpjm_desa'] = array(
+
+$config['rp_rkp_desa'] = array(
     'colModel' => array(
-        'id_rancangan_rpjm_desa' => array('ID', 30, TRUE, 'center', 0),
-        'bidang' => array('Bidang', 100, TRUE, 'center', 2),
-        'sub_bidang' => array('Sub Bidang', 120, TRUE, 'center', 2),
+        'id_m_rkp' => array('ID', 30, TRUE, 'center', 0),
+        'id_m_rancangan_rpjm_desa' => array('ID RPJM', 30, TRUE, 'center', 0, 1),
+        'rkp_tahun' => array('Tahun Anggaran', 100, TRUE, 'center', 2, 1),
+        'bidang' => array('Bidang', 120, TRUE, 'center', 2),
         'jenis_kegiatan' => array('Jenis Kegiatan', 120, TRUE, 'center', 2),
-        'lokasi_rt_rw' => array('Lokasi<br />(RT/RW<br />/Dusun)', 60, TRUE, 'center', 2),
-        'prakiraan_volume' => array('Prakiraa<br />Volume', 60, TRUE, 'center', 2),
+        'lokasi' => array('Lokasi', 60, TRUE, 'center', 2),
+        'volume' => array('Volume', 60, TRUE, 'center', 2),
         'sasaran_manfaat' => array('Sasaran Manfaat', 120, TRUE, 'center', 2),
-        'tahun_pelaksanaan_1' => array('Tahun<br />Pelaksanaan 1', 50, TRUE, 'center', 2),
-        'tahun_pelaksanaan_2' => array('Tahun<br />Pelaksanaan 2', 50, TRUE, 'center', 2),
-        'tahun_pelaksanaan_3' => array('Tahun<br />Pelaksanaan 3', 50, TRUE, 'center', 2),
-        'tahun_pelaksanaan_4' => array('Tahun<br />Pelaksanaan 4', 50, TRUE, 'center', 2),
-        'tahun_pelaksanaan_5' => array('Tahun<br />Pelaksanaan 5', 50, TRUE, 'center', 2),
-        'tahun_pelaksanaan_6' => array('Tahun<br />Pelaksanaan 6', 50, TRUE, 'center', 2),
-        'jumlah_biaya' => array('Prakiraan<br />Jumlah Biaya<br />(Rp)', 80, TRUE, 'center', 2),
-        'sumber_dana' => array('Sumber<br />Pembiayaan', 100, TRUE, 'center', 2),
+        'waktu_pelaksanaan' => array('Waktu Pelaksanaan', 50, TRUE, 'center', 2),
+        'jumlah_biaya' => array('Jumlah Biaya', 80, TRUE, 'center', 2),
         'pelaksanaan_swakelola' => array('Swakelola', 100, TRUE, 'center', 2),
         'pelaksanaan_kerjasama_antar_desa' => array('Kerjasama<br />Antar Desa', 100, TRUE, 'center', 2),
         'pelaksanaan_kerjasama_pihak_ketiga' => array('Kerjasama<br />Pihak Ketiga', 100, TRUE, 'center', 0),
-//        'aksi' => array('AKSI', 120, FALSE, 'center', 0)
     ),
     'buttons' => NULL,
     'gridParams' => array(
@@ -46,24 +35,23 @@ $config['rp_rancangan_rpjm_desa'] = array(
 //        'rpOptions' => '[10,20,30,40]', /* jumlah opsi untuk mengganti jumlah halaman */
 //        'pagestat' => 'Displaying: {from} to {to} of {total} items.',
 //        'blockOpacity' => 0.5,
-        'title' => 'Detail RPJM Desa ',
+        'title' => 'Detail RKP Desa',
         'showTableToggleBtn' => FALSE
     )
 );
-
-
-$config['rp_master_rancangan_rpjm_desa'] = array(
+$config['rp_master_rkp_desa'] = array(
     'colModel' => array(
-        'id_m_rancangan_rpjm_desa' => array('ID', 30, TRUE, 'center', 0),
+        'id_m_rkp' => array('ID', 30, TRUE, 'center', 0),
+        'id_m_rancangan_rpjm_desa' => array('ID RPJM', 30, TRUE, 'center', 0, 1),
         'tahun_awal' => array('Tahun Awal', 100, TRUE, 'center', 2, 1),
         'tahun_akhir' => array('Tahun Akhir', 120, TRUE, 'center', 2, 1),
         'tahun_anggaran' => array('Tahun Anggaran', 120, TRUE, 'center', 2),
-        'nama_file' => array('Nama File', 120, TRUE, 'center', 2, 1),
         'total_bidang_1' => array('Total Bidang 1', 60, TRUE, 'center', 2),
         'total_bidang_2' => array('Total Bidang 2', 60, TRUE, 'center', 2),
         'total_bidang_3' => array('Total Bidang 3', 120, TRUE, 'center', 2),
         'total_bidang_4' => array('Total Bidang 4', 50, TRUE, 'center', 2),
         'total_keseluruhan' => array('Total Jumlah', 80, TRUE, 'center', 2),
+        'rkp_tahun' => array('RKP Tahun', 120, TRUE, 'center', 2),
         'tanggal_disusun' => array('Tanggal Disusun', 100, TRUE, 'center', 2, 1),
         'disusun_oleh' => array('Disusun Oleh', 100, TRUE, 'center', 2, 1),
         'kepala_desa' => array('Nama<br />Kepala Desa', 100, TRUE, 'center', 2, 1),
@@ -82,7 +70,6 @@ $config['rp_master_rancangan_rpjm_desa'] = array(
 //        array('separator'),
         array('Tambah', 'add', 'btn'),
         array('separator'),
-        array('Import Excel', 'add', 'btn'),
 //        array('separator'),
 //        array('Delete Selected Items', 'delete', 'btn'),
 //        array('separator')
@@ -95,7 +82,7 @@ $config['rp_master_rancangan_rpjm_desa'] = array(
         'rpOptions' => '[10,20,30,40]', /* jumlah opsi untuk mengganti jumlah halaman */
         'pagestat' => 'Displaying: {from} to {to} of {total} items.',
 //        'blockOpacity' => 0.5,
-        'title' => 'Rancangan RPJM Desa',
+        'title' => 'RKP Desa',
 //        'showTableToggleBtn' => FALSE
     )
 );
